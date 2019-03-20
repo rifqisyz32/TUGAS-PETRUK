fa1bdd5fd321d84d6b67e48354bf12e4abc3e06c
Rifqi Syamsu Z <48714117+rifqisyz32@users.noreply.github.com>
2019-03-20 21:53:19 +0700

    Update Find-word.cpp

--- Find-word.cpp
+++ Find-word.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 int main () 
 {
-cout<<"\t\t\t\t\t\t    ----------- \n";
+	cout<<"\t\t\t\t\t    ------------------------ \n";
+	cout<<"\t\t\t\t\t   |  PROGRAM MENCARI KATA  |\n";
+	cout<<"\t\t\t\t\t    ------------------------ \n";
+	cout<<"\n\t\t\t\t\t              Oleh\n\n";
+	cout<<"\n\n\t\t\t\t\t     Rifqi Syamsu Zulkarnain\n\t\t\t\t\t\t    1817051032\n\n";
+	cout<<"\n\n\t\t\t\t\t         Rifqi Adiyatma\n\t\t\t\t\t\t    1817051055\n";
+	cout<<"\n\n\t\t\t\t\t      Tommy Ferdiansyah A \n\t\t\t\t\t\t    1817051072\n\n";
+	cout<<"\n\n\t\t\t\t\t     Gilang Rahman Affandi\n\t\t\t\t\t\t    1817051075\n";
+	system ("pause");
+	system ("cls");
+	cout<<"\t\t\t\t\t\t    ----------- \n";
 	cout<<"\t\t\t\t\t\t   | FIND WORD |\n";
 	cout<<"\t\t\t\t\t\t    ----------- \n";
 	char find[15][15]=  { {'t','g','b','w','w','i','n','t','e','r','w','s','e','s','n'},
